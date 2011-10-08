45bfcc0ca55f38e6a325969dabbdf259c89040ec
theteddev@gmail.com <theteddev@gmail.com@c16766be-c56a-0b3e-7437-2a3eaa951191>
2011-10-08 21:02:46 +0000

    refactored packages

---
+++ trunk/src/com/ted/aggredata/server/services/LocationService.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2011. The Energy Detective. All Rights Reserved
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ted.aggredata.server.services;
+
+import com.ted.aggredata.model.Group;
+import com.ted.aggredata.model.Location;
+import com.ted.aggredata.model.User;
+
+import java.util.List;
+
+/**
+ * Public interface to the user management service
+ */
+public interface LocationService {
+
+    /**
+     * Adds a new location to the system
+     * @param user
+     * @param location
+     */
+    public void addLocation(User user, Location location);
+
+    /**
+     * Removes a location from the system
+     * @param location
+     */
+    public void removeLocation(Location location);
+
+
+    /**
+     * Updates an existing location
+     * @param location
+     */
+    public void updateLocation(Location location);
+
+
+    /**
+     * Retrieves a list of locations for a given user
+     * @param user
+     * @return
+     */
+    public List<Location> getLocations(User user);
+
+    /**
+     * Retrieves a list of locations for a given group
+     * @param group
+     * @return
+     */
+    public List<Location> getLocations(Group group);
+
+    /**
+     * Returns the location w/ the given description for the specified user
+     * @param user
+     * @param description
+     * @return
+     */
+    public Location getLocation(User user, String description);
+
+
+
+    /**
+     * Adds a location to the given group
+     * @param location
+     * @param group
+     */
+    public void addLocationToGroup(Location location, Group group);
+
+
+    /**
+     * Removes a location from the given group
+     * @param location
+     * @param group
+     */
+    public void removeLocationFromGroup(Location location, Group group);
+
+
+
+}
---
+++ trunk/src/com/ted/aggredata/server/services/impl/LocationServiceImpl.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2011. The Energy Detective. All Rights Reserved
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ted.aggredata.server.services.impl;
+
+import com.ted.aggredata.model.Group;
+import com.ted.aggredata.model.Location;
+import com.ted.aggredata.model.User;
+import com.ted.aggredata.server.dao.LocationDAO;
+import com.ted.aggredata.server.services.LocationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/***
+ * Implementation of the GroupService interface
+ */
+
+@Transactional
+public class LocationServiceImpl implements LocationService {
+
+    @Autowired
+      protected LocationDAO locationDAO;
+      Logger logger = LoggerFactory.getLogger(getClass());
+
+    public void addLocation(User user, Location location) {
+        logger.info("User " + user + " + is adding " + location);
+        location.setUserId(user.getId());
+        locationDAO.create(location);
+    }
+
+    public void removeLocation(Location location) {
+        logger.info("removing " + location);
+        locationDAO.removeGroupLocations(location);
+        locationDAO.delete(location);
+    }
+
+    public void updateLocation(Location location) {
+        logger.info("Updating " + location);
+        locationDAO.save(location);
+    }
+
+    public List<Location> getLocations(User user) {
+        return locationDAO.getByUser(user);
+    }
+
+    public List<Location> getLocations(Group group) {
+        return locationDAO.getByGroup(group);
+    }
+
+    public Location getLocation(User user, String description) {
+        return locationDAO.getLocation(user, description);
+    }
+
+    public void addLocationToGroup(Location location, Group group) {
+        logger.info("Adding " + location + " to " + group);
+        locationDAO.addLocationToGroup(location, group);
+    }
+
+    public void removeLocationFromGroup(Location location, Group group) {
+        logger.info("Removing " + location + " from " + group);
+        locationDAO.removeLocationFromGroup(location, group);
+    }
+}
