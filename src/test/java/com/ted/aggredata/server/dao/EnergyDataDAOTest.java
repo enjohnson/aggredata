4a8f0b7755be6f8fe9131f152f9c986b4eaac7a0
theteddev <theteddev@c16766be-c56a-0b3e-7437-2a3eaa951191>
2012-03-23 20:47:33 +0000

    Added unit test for EnergyData DAO

---
+++ src/test/java/com/ted/aggredata/server/dao/EnergyDataDAOTest.java
@@ -0,0 +1,111 @@
+package com.ted.aggredata.server.dao;
+
+import com.ted.aggredata.model.EnergyData;
+import com.ted.aggredata.model.Gateway;
+import com.ted.aggredata.model.MTU;
+import com.ted.aggredata.model.User;
+import com.ted.aggredata.server.util.TestUtil;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml",
+        "classpath:applicationContext-Test.xml"
+})
+public class EnergyDataDAOTest {
+
+
+
+    
+    @Autowired
+    GroupDAO groupDAO;
+
+    @Autowired
+    UserDAO userDAO;
+
+    @Autowired
+    GatewayDAO gatewayDAO;
+
+    @Autowired
+    MTUDAO mtuDAO;
+
+    @Autowired
+    EnergyDataDAO energyDataDAO;
+
+
+    User gatewayOwner1 = null;
+
+
+    @Test
+    public void testEnergyData() {
+
+        Gateway oldGateway = gatewayDAO.findById(GatewayDAOTest.GATEWAY_TEST_ID);
+        if (oldGateway != null) gatewayDAO.delete(oldGateway);
+
+        
+        //Set up data
+        User user = new User();
+        user.setUsername(TestUtil.getUniqueKey());
+        user.setPassword(TestUtil.getUniqueKey());
+        user = userDAO.create(user);
+        
+        
+        Gateway gateway = new Gateway();
+        gateway.setId(GatewayDAOTest.GATEWAY_TEST_ID);
+        gateway.setUserAccountId(user.getId());
+        gateway.setDescription(TestUtil.getUniqueKey());
+        gateway.setState(true);
+        try {
+            gateway = gatewayDAO.create(gateway);
+        } catch (Exception ex) {
+            Assert.fail("Gateway Exists already");
+        }
+
+        MTU mtu = new MTU();
+        mtu.setId(MTUDAOTest.MTU_TEST_ID);
+        mtu.setGatewayId(gateway.getId());
+        mtu.setDescription(TestUtil.getUniqueKey());
+        mtu.setType(MTU.MTUType.LOAD);
+        mtu = mtuDAO.create(gateway, mtu);
+        
+
+        //Create 10 rows of data
+        for (int i=0; i < 10; i++)
+        {
+            EnergyData energyData = new EnergyData();
+            energyData.setMtuId(mtu.getId());
+            energyData.setEnergy(10000d);
+            energyData.setRate(1.0000d);
+            energyData.setTimestamp(10000+i);
+            energyDataDAO.create(energyData);
+        }
+
+
+        List<EnergyData> energyDataList = energyDataDAO.findByMTU(mtu, 10001, 10005);
+        Assert.assertEquals(energyDataList.size(), 4);
+        
+        for (int i=0; i < 4; i++)
+        {
+            Assert.assertEquals(new Integer(10001+i), energyDataList.get(i).getTimestamp());
+            
+        }
+
+        energyDataDAO.removeEnergyData(mtu);
+        energyDataList = energyDataDAO.findByMTU(mtu, 10001, 10005);
+
+        Assert.assertEquals(0, energyDataList.size());
+
+        mtuDAO.delete(mtu);
+        gatewayDAO.delete(gateway);
+        userDAO.delete(user);
+        
+        
+
+    }
+}
